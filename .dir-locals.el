8b100e19b4f0609ad330c63af2afb4646aaca3a2
c4droid <c4droid@foxmail.com>
2025-01-02 01:48:22 +0800

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (electric-pair-mode))))
+ (scheme-mode . ((eval . (guix-devel-mode)))))
